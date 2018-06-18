5b20c3c4712a6b72059004f44ccc31828efa3bfd
Clayton Stone <devcircus@outlook.com>
2018-06-18 10:01:46 -0500

    Add docblocks to ServiceTranslator and interface.

--- src/Contracts/ServiceTranslatorContract.php
+++ src/Contracts/ServiceTranslatorContract.php
@@ -4,5 +4,12 @@ namespace BrightComponents\Service\Contracts;
 
 interface ServiceTranslatorContract
 {
-    public function translateServiceToHandler($service) : string;
+    /**
+     * Translate the given service definition to the matching handler.
+     *
+     * @param  mixed  $service
+     *
+     * @return string
+     */
+    public function translateServiceToHandler($service): string;
 }
