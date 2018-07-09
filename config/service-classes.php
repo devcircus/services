c2e5cf6a33334e4fd5b42492c2ef2212e7b3f0d0
Clayton Stone <phpcircus@gmail.com>
2018-07-09 14:48:06 -0500

    Update comment wording
    Rename "definition" to "Service", as the definition construct no longer exists.

--- config/service-classes.php
+++ config/service-classes.php
@@ -36,7 +36,7 @@ return [
     | Duplicate Suffixes
     |--------------------------------------------------------------------------
     |
-    | If you have a definition suffix set in the config and try to generate a Service that also includes the suffix,
+    | If you have a Service suffix set in the config and try to generate a Service that also includes the suffix,
     | the package will recognize this duplication and rename the Service to remove the extra suffix.
     | This is the default behavior. To override and allow the duplication, change to false.
     |
