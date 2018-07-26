400fd1d0dbf50233965b669333a5ecc4e2491aea
Clayton Stone <devcircus@outlook.com>
2018-07-26 15:50:23 -0500

    Fix command call in tests.

--- tests/Integration/ServiceCommandTest.php
+++ tests/Integration/ServiceCommandTest.php
@@ -11,7 +11,7 @@ class ServiceCommandTest extends IntegrationTestCase
     /** @test */
     public function bright_service_command_makes_service_with_correct_methods()
     {
-        Artisan::call('bright:service', ['name' => 'MyService']);
+        Artisan::call('adr:service', ['name' => 'MyService']);
 
         include_once base_path().'/app/Services/MyService.php';
 
